a7b7d632d4e172b58b76da2ad28dc7a491cf41b7
Michael Truschelev <mishele20@mail.ru>
2021-03-11 19:48:50 +0300

    95% in sortOrders

--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, getSortFunction, sortByDate, sortOrders, sortTypes} from './sortOrders';
+
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -20,5 +21,117 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('left item lesser than right', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		expect(sortByItemCount(order1, order2)).toBe(-1);
+	})
+
+	it('left item greater than right', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		expect(sortByItemCount(order1, order2)).toBe(1);
+	})
+
+	it('not an objects', () => {
+		expect(sortByItemCount([], [])).toBe(0);
+	})
+
+	it('without args', () => {
+		expect(sortByItemCount()).toBe(0);
+	})
+
+	it('without items', () => {
+		expect(sortByItemCount({}, {items: []})).toBe(0);
+	})
+
+	it('right type returns date', () => {
+		expect(getSortFunction('date')).toBe(sortByDate);
+	})
+
+	it('right type returns count', () => {
+		expect(getSortFunction('count')).toBe(sortByItemCount);
+	})
+
+	it('dates equal', () => {
+		const order1 = {
+			date: 1552481120000,
+		};
+
+		const order2 = {
+			date: 1552481120000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(0);
+	})
+
+	it('left lesser than right date', () => {
+		const order1 = {
+			date: 155248112000,
+		};
+
+		const order2 = {
+			date: 1552481120000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(1);
+	})
+
+	it('left greater than right date', () => {
+		const order1 = {
+			date: 155248112000,
+		};
+
+		const order2 = {
+			date: 1552481000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(-1);
+	})
+
+	it('not an objects in date sort', () => {
+		expect(sortByDate([], [])).toBe(0);
+	})
+
+	it('no args in date sort', () => {
+		expect(sortByDate()).toBe(0);
+	})
+
+	it('no date args in date sort', () => {
+		expect(sortByDate({}, {date: 1})).toBe(0);
+	})
+
+	it('call sort func', () => {
+		const orders = [
+			{items: ['item1', 'item2']},
+			{items: ['item1', "item2", "item3"]}
+		];
+
+		const func = jest.fn()
+		sortOrders(orders, func)
+		expect(func).toHaveBeenCalled();
+	})
+
+	it('not call sort func', () => {
+		const orders = [];
+
+		const func = jest.fn()
+		sortOrders(orders, func)
+		expect(func).not.toHaveBeenCalled();
+	})
+
 });
 
